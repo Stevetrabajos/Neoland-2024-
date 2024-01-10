a040036f8e836106eefd5dde8bd65868c0271b49
Stevetrabajos <markmarticatala88@gmail.com>
2024-01-10 16:08:42 +0100

    iteration1.3

--- JS/app.js
+++ JS/app.js
@@ -8,7 +8,7 @@ Iteración #1: Variables
 
 //1.1 ---------------------------------------------
 
-let myFavoriteHero = "Hulk";
+let myFavoriteHero = "Hulk;"
 
 //1.2 ---------------------------------------------
 
@@ -71,7 +71,7 @@ const toy2 = {name: 'Rallo mcKing', price: 29};
 
 let suma = toy1.price + toy2.price;
 
-console.log (suma);
+console.log ("suma juguetes" , suma);
 
 //1.4 ---------------------------------------------
 
@@ -80,12 +80,12 @@ const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
 const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
 
 globalBasePrice = 25000;
-//console.log (globalBasePrice)
+console.log (globalBasePrice)
 
 car1.finalPrice = car1.basePrice + globalBasePrice;
 car2.finalPrice = car2.basePrice + globalBasePrice;
-/*console.log (car1.finalPrice);
-console.log (car2.finalPrice);*/
+console.log (car1.finalPrice);
+console.log (car2.finalPrice);
 
 
 /*
@@ -126,7 +126,7 @@ console.log (division);
 let a = 15;
 let b = 9;
 
-let resto = a % b;
+let resto = a/b;
 console.log (resto);
 
 //1.4 ---------------------------------------------
@@ -135,7 +135,7 @@ let p = 10;
 let j = 5;
 
 let o = p + j;
-//console.log (o);
+console.log (o);
 
 //1.5 ---------------------------------------------
 
@@ -143,4 +143,26 @@ let c = 10;
 let m = 5;
 
 let i = c * m;
-//console.log (i);
+console.log (i);
+
+/*
+1.1 Consigue el valor "HULK" del array de avengers y muestralo por consola.
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+1.2 Cambia el primer elemento de avengers a "IRONMAN"
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+1.3 console numero de elementos en el array usando la propiedad correcta de Array.
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+1.4 Añade 2 elementos al array: "Morty" y "Summer". 
+Muestra en consola el último personaje del array
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+
+1.5 Elimina el último elemento del array y muestra el primero y el último por consola.
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+1.6 Elimina el segundo elemento del array y muestra el array por consola.
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+*/
+
