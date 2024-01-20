78645e4a4c0d4bd8cca120d31a2d3325dcfd6b54
Stevetrabajos <markmarticatala88@gmail.com>
2024-01-20 20:06:33 +0100

    Iteration 5

--- JS/loops/app.js
+++ JS/loops/app.js
@@ -8,3 +8,46 @@ const incluye = (products) =>{
     return array
     }
     console.log(incluye(products))
+
+    //------------------------------------------Iteration 2
+
+
+
+//------------------------------------------------------Iteration 3
+
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']
+
+for (let places of placesToTravel){
+    console.log(places)   
+}
+
+//-----------------------------------------------------Iteration 4
+const alien = {
+    name: 'Wormuck',
+    race: 'Cucusumusu',
+    planet: 'Eden',
+    weight: '259kg'
+}
+for ( let caracteristicas in alien){
+    console.log("el alien tiene la característica " + caracteristicas + ": " + alien[caracteristicas])
+}
+
+//-------------------------------------------------- Iteration #4
+
+const placesToTravel2 = [{
+    id: 5, name: 'Japan'},
+     {id: 11, name: 'Venecia'},
+      {id: 23, name: 'Murcia'},
+       {id: 40, name: 'Santander'},
+        {id: 44, name: 'Filipinas'},
+         {id: 59, name: 'Madagascar'}]
+
+const poli = (placesToTravel2)=>{
+    let Totravel=[]
+for (let i=0; i<placesToTravel2.length; i++){
+if(placesToTravel2[i].id!==11&&placesToTravel2[i].id!==40)
+Totravel.push(placesToTravel2[i]);
+}
+return Totravel
+}
+console.log(poli(placesToTravel2))
