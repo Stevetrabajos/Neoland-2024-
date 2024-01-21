3a202c9b763f7c0f0d73c8d785aa99594a3325c2
Stevetrabajos <markmarticatala88@gmail.com>
2024-01-21 22:36:09 +0100

    Iteration 7

--- JS/loops/app.js
+++ JS/loops/app.js
@@ -64,11 +64,29 @@ const toys = [
     {id: 40, name: 'El gato con Guantes'},
     {id: 40, name: 'El gato felix'}
     ]
-        const newtoys= [];
+        let newtoys= [];
 for(const word of toys){
 if (!word.name.includes("gato")){
 newtoys.push(word);
 }
-
 }
 console.log(newtoys)
+
+//-------------------------------Iteration #7
+
+const popularToys = [];
+const toys2 = [
+	{id: 5, name: 'Buzz MyYear', sellCount: 10}, 
+	{id: 11, name: 'Action Woman', sellCount: 24}, 
+	{id: 23, name: 'Barbie Man', sellCount: 15}, 
+	{id: 40, name: 'El gato con Guantes', sellCount: 8},
+	{id: 40, name: 'El gato felix', sellCount: 35}
+]
+
+for (venta of toys2){
+ if(venta.sellCount>=15){
+ popularToys.push(toys2)
+
+return popularToys   
+ }
+}
