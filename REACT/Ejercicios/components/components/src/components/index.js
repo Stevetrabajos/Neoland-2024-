8e9383996d4d7157e9bc8fa7bdf0e14d11825b2c
Stevetrabajos <markmarticatala88@gmail.com>
2024-02-23 11:59:17 +0100

    ej react

---
+++ REACT/Ejercicios/components/components/src/components/index.js
@@ -0,0 +1,7 @@
+export * from "./Header.jsx";
+export * from "./Title.jsx";
+export * from "./Paragraph.jsx";
+export * from "./Main.jsx";
+export * from "./Image.jsx";
+export * from "./Footer.jsx";
+export * from "./SubTitle.jsx";
